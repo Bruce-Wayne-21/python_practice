a088c5488ae3101371f59b0d1af3fa4e053923ac
Sajilan UCA <sajilan@unicornconnectedapps.com>
2025-05-17 23:29:58 +0530

    first comit

---
+++ variable.py
@@ -0,0 +1,12 @@
+age_input=input("enter your name")
+age=int(input("Enter your age "))
+
+if age <0:
+    print("plese enter the valid value")
+elif age <10:
+    print(" you are minor")
+elif age < 20:
+    print("You are major")
+else:
+    print("hi i am ssajilan ")
+
